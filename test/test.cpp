a97c6c0106a0a6cfcf7e2188d1699db9b7dba6be
Filipe Fortunato <filipe.fortunato@heig-vd.ch>
2019-05-17 11:06:44 +0200

    init repo

---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by filipe on 5/17/19.
+//
+
+// COPYRIGHT 2019 <TBD>
+
+#include <gtest/gtest.h>
+#include "test.h"
+
+int main(int ac, char *av[]) {
+  testing::InitGoogleTest(&ac, av);
+  return RUN_ALL_TESTS();
+}
---
+++ test/test.h
@@ -0,0 +1,17 @@
+//
+// Created by filipe on 5/17/19.
+//
+
+// COPYRIGHT 2019 <TBD>
+#ifndef INCLUDE_test_H_
+#define INCLUDE_test_H_
+
+#include <gtest/gtest.h>
+
+/*
+TEST(TOTO, TATA) {
+
+}
+*/
+
+#endif // INCLUDE_test_H_
